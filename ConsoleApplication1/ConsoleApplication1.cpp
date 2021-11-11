cf8c4e901d8ff87a7b18d5c6ed8453d4541557bb
Комп <@LAPTOP-LIRLQVJD>
2021-11-11 10:44:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	while ("") {
+		int	c, k, j, n;
+
+		setlocale(LC_ALL, "Russian");
+		cout << "Введите значение k: ";
+
+		cin >> k;
+
+		c = n = 0;
+		do {
+			for (n++, c++, j = n; j /= 10; c++);
+		} while (c < k);
+		for (c -= k; c--; n /= 10);
+
+		cout << "k-ая цифра последовательности: " << n % 10 << endl;
+		break;
+	}
+	cin.get();
+	return 0;
+}
